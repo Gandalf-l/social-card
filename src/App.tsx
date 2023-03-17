a83beacde104a76c1684c2fe877aa770c7e93221
Danylo <danylo.averbakh@gmail.com>
2023-03-17 14:38:35 +0000

    add loader

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import './App.scss';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
+import Loader from './components/Loader/Loader';
+import { observer } from 'mobx-react-lite';
 
-function App() {
+import loaderStore from './store/LoaderStore';
+
+const App = observer(() => {
   return (
     <>
-      <Header/>
-      <Main/>
+      {loaderStore.isLoad && <Loader />}
+      <Header />
+      <Main />
     </>
   );
-}
+});
 
 export default App;
---
+++ src/store/LoaderStore.tsx
@@ -0,0 +1,19 @@
+import { makeAutoObservable } from 'mobx';
+
+class LoaderStore {
+  isLoad = false;
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  loadStart() {
+    this.isLoad = true;
+  }
+
+  loadEnd() {
+    this.isLoad = false;
+  }
+}
+
+export default new LoaderStore()
